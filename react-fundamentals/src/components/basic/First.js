5512420b7f863274cfbf54a7e736f014653c6923
charmingruby <mrrubycanal3105@gmail.com>
2022-03-17 09:58:49 -0300

    first component using react

---
+++ react-fundamentals/src/components/basic/First.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Primeiro(){
+    const msg = 'Welcome to my first React component.'
+    return (
+        <div>
+            <h2>First React Component</h2>
+            <p>{ msg }</p>
+        </div>
+    )       
+}
---
+++ react-fundamentals/src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import './index.css'
+
+import First from './components/basic/First'
+
+ReactDOM.render(
+    <div>
+        <First></First>
+    </div>,
+    document.getElementById('root')
+)
