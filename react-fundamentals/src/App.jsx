6a1aa60b535339c374349c4fcd1e652fed53b60b
charmingruby <mrrubycanal3105@gmail.com>
2022-03-21 11:23:26 -0300

    card component

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -1,22 +1,36 @@
 import React from 'react'
 
+import './App.css'
+
 import Lonely from './components/basic/Lonely'
 import LonelyWithParameter from './components/basic/LonelyWithParameter'
 import LonelyFragment from './components/basic/LonelyFragment'
 import RandomNumber from './components/basic/RandomizedNum'
+import Card from './components/layouts/Card'
 
-export default () => 
-        <div class="app">
-            <h1>React Fundamentals</h1>
+export default () =>
+    <div class="App">
+        <h1>React Fundamentals</h1>
 
-            <RandomNumber
-                min = {7}
-                max = {18} />
+        <div className="Cards">
+            <Card title="#4 - Random Number">
+                <RandomNumber
+                    min={7}
+                    max={18} />
+            </Card>
 
-            <Lonely/>
-            
-            <LonelyWithParameter
-                title='Lonely component w/ parameters !'
-                subtitle='Lonely component made with some parameters' />
-            <LonelyFragment />
+            <Card title="#3 - Lonely Fragment">
+                <LonelyFragment />
+            </Card>
+
+            <Card title="#2 - Lonely w/ Parameter">
+                <LonelyWithParameter
+                    title='Lonely component w/ parameters !'
+                    subtitle='Lonely component made with some parameters' />
+            </Card>
+
+            <Card title="#1 - Lonely">
+                <Lonely />
+            </Card>
         </div>
+    </div>
--- react-fundamentals/src/components/basic/Lonely.jsx
+++ react-fundamentals/src/components/basic/Lonely.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Lonely() {
     return (
         <div class="lonely-content">
-            <h2><strong>Lonely Component !</strong></h2>
+            <h2>Lonely Component !</h2>
             <p>My first component that i have made alone.</p>
         </div>
     )
